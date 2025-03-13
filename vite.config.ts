3bb9e6508c191a1de413bf659ee4f5f07013d5ec
Adam Robert Turman <67024033+adamrturman@users.noreply.github.com>
2025-03-13 14:38:19 -0500

    fix: run prettier (#5)
    * fix: run prettier
    
    * config: add npm run check

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +12,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
-})
+  },
+});
