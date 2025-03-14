3ceed6637c33524e2bab7719667fbd2365e2fec1
adamrturman <adamrturman@gmail.com>
2025-03-14 12:41:38 -0500

    refactor: move CSS into landing content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import styles from './App.module.css';
 import { LandingContent } from './LandingContent';
 
 export const App = () => {
   return (
-    <div className={styles.landingContent}>
+    <>
       <LandingContent />
-    </div>
+    </>
   );
 };
--- src/LandingContent.tsx
+++ src/LandingContent.tsx
@@ -1,12 +1,19 @@
 import styles from './LandingContent.module.css';
-import HardDiamond from './assets/hardDiamond.svg';
-import HardAILogo from './assets/hardAILogo.svg';
+import hardDiamondLogo from './assets/hardDiamondLogo.svg';
+import hardAILogo from './assets/hardAILogo.svg';
 
 export const LandingContent = () => {
   return (
-    <div className={styles.logoWrapper}>
-      <img height={100} width={100} src={HardDiamond} alt="Hard Diamond logo" />
-      <img height={75} width={200} src={HardAILogo} alt="Hard AI logo" />
+    <div className={styles.landingContent}>
+      <div className={styles.logoWrapper}>
+        <img
+          height={100}
+          width={100}
+          src={hardDiamondLogo}
+          alt="Hard Diamond logo"
+        />
+        <img height={75} width={200} src={hardAILogo} alt="Hard AI logo" />
+      </div>
     </div>
   );
 };
