3da77a2353400109b51d0671e11b9cf3231f3e15
adamrturman <adamrturman@gmail.com>
2025-03-14 12:59:42 -0500

    style: reduce blank space

--- src/LandingContent.tsx
+++ src/LandingContent.tsx
@@ -6,13 +6,8 @@ export const LandingContent = () => {
   return (
     <div className={styles.landingContent}>
       <div className={styles.logoWrapper}>
-        <img
-          height={100}
-          width={100}
-          src={hardDiamondLogo}
-          alt="Hard Diamond logo"
-        />
-        <img height={75} width={200} src={hardAILogo} alt="Hard AI logo" />
+        <img width={100} src={hardDiamondLogo} alt="Hard Diamond logo" />
+        <img width={200} src={hardAILogo} alt="Hard AI logo" />
       </div>
     </div>
   );
