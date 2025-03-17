0bb68204c7ff2932a4e0b41db2173f87390efb38
adamrturman <adamrturman@gmail.com>
2025-03-17 12:35:26 -0500

    feat: add input
    - remove diamond logo (to be in the navbar later)
    - adjust smoke test

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -8,11 +8,9 @@ test('renders the page with correct displayed & meta title', async ({
 
   await expect(page).toHaveTitle(/Hard AI/);
 
-  const diamondLogo = page.getByRole('img', { name: 'Hard Diamond logo' });
   const hardAILogo = page.getByRole('img', { name: 'Hard AI logo' });
   const welcomeText = page.getByText('How can I help you with Web3?');
 
-  await expect(diamondLogo).toBeVisible();
   await expect(hardAILogo).toBeVisible();
   await expect(welcomeText).toBeVisible();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { LandingContent } from './LandingContent';
+import { ChatInput } from './ChatInput';
+import styles from './App.module.css';
 
 export const App = () => {
   return (
-    <>
-      <LandingContent />
-    </>
+    <div className={styles.chatview}>
+      <div className={styles.controlsContainer}>
+        <LandingContent />
+        <ChatInput />
+      </div>
+    </div>
   );
 };
