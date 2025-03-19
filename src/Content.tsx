b4e95e00c6f7db8a26d296d5aa4ad881d6b299af
samcode206 <hadi.sameem.dev@gmail.com>
2025-03-19 14:03:04 -0700

    feat: sanatizeContent

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,18 +1,63 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import styles from './Content.module.css';
+import { sanitizeContent } from './utils/sanitize';
 
 export interface ContentProps {
   content: string;
   role: string;
 }
 
-export const ContentComponent = ({ content, role }: ContentProps) => {
+export const ContentComponent = ({
+  content,
+
+  role,
+}: ContentProps) => {
+  const [hasError, setHasError] = useState(false);
+  const [sanitizedContent, setSanitizedContent] = useState<string>('');
+
+  useEffect(() => {
+    let cancel = false;
+
+    const handleRender = async () => {
+      if (content === '') {
+        setSanitizedContent('');
+        return;
+      }
+
+      try {
+        const updatedContent = await sanitizeContent(content);
+
+        if (!cancel) {
+          setSanitizedContent(updatedContent);
+        }
+      } catch (error) {
+        // TODO: This is noisy in tests
+        console.error(error);
+        if (!cancel) {
+          setHasError(true);
+        }
+      }
+    };
+
+    handleRender();
+
+    return () => {
+      cancel = true;
+    };
+  }, [content]);
+
+  if (hasError) {
+    return <span>Error: Could not render content!</span>;
+  }
+
   return (
-    <div data-chat-role={role}>
-      <span
-        className={styles.content}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+    <div data-testid="conversation-message" data-chat-role={role}>
+      {sanitizedContent !== '' && (
+        <span
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+        />
+      )}
     </div>
   );
 };
