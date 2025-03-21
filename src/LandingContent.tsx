e942b14700d04c8d12e0903f83b40dfed7b61539
adamrturman <adamrturman@gmail.com>
2025-03-21 09:22:26 -0500

    style: slightly taller logo to match

--- src/LandingContent.tsx
+++ src/LandingContent.tsx
@@ -5,7 +5,7 @@ export const LandingContent = () => {
   return (
     <div className={styles.landingContent}>
       <div className={styles.logoWrapper}>
-        <img width={200} src={hardAILogo} alt="Hard AI logo" />
+        <img height={50} width={200} src={hardAILogo} alt="Hard AI logo" />
         <h2 className={styles.introText}>How can I help you with Web3?</h2>
       </div>
     </div>
