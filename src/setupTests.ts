e33304c449631c72a9cbf04b7dd3d6be564d1e60
adamrturman <adamrturman@gmail.com>
2025-04-03 16:50:09 -0500

    fix: dropped commits

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,10 +2,11 @@ import '@testing-library/jest-dom';
 import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
-vi.mock('lib-kava-ai', () => ({
-  useIsMobileLayout: vi.fn().mockReturnValue(false),
-}));
-
+vi.mock('lib-kava-ai', async () => {
+  return {
+    useIsMobileLayout: vi.fn(),
+  };
+});
 afterEach(() => {
   cleanup();
 });
