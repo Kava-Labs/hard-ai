c3950c0cd120765013a187a67ab81dad64e72577
adamrturman <adamrturman@gmail.com>
2025-04-03 16:51:45 -0500

    refactor: cleaner diff

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,11 +2,10 @@ import '@testing-library/jest-dom';
 import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
-vi.mock('lib-kava-ai', async () => {
-  return {
-    useIsMobileLayout: vi.fn(),
-  };
-});
+vi.mock('lib-kava-ai', async () => ({
+  useIsMobileLayout: vi.fn().mockReturnValue(false),
+}));
+
 afterEach(() => {
   cleanup();
 });
