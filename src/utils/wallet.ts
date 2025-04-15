064fd25c846bd2c8cf7a390e11b25d1322eab489
adamrturman <adamrturman@gmail.com>
2025-04-15 16:54:41 -0500

    fix: cleaner diff

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,6 +1,6 @@
-import { PromotedWallet } from '../types';
 import metamaskLogo from '../assets/MetaMask-icon-fox.svg';
 import hotWalletLogo from '../assets/HOT Wallet Short.svg';
+import { PromotedWallet } from '../types';
 
 export const PROMOTED_WALLETS: PromotedWallet[] = [
   {
