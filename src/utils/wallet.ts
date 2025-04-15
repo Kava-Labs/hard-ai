8f0afa182f06b7f1eb7c0e229d04b05487ca86b1
adamrturman <adamrturman@gmail.com>
2025-04-15 16:54:03 -0500

    fix: svg import

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,6 +1,6 @@
 import { PromotedWallet } from '../types';
-import metamaskLogo from '*.svg';
-import hotWalletLogo from '*.svg';
+import metamaskLogo from '../assets/MetaMask-icon-fox.svg';
+import hotWalletLogo from '../assets/HOT Wallet Short.svg';
 
 export const PROMOTED_WALLETS: PromotedWallet[] = [
   {
