60c83b6a044fc690f597271b9db65dadf9e40300
samcode206 <hadi.sameem.dev@gmail.com>
2025-04-18 12:37:15 -0700

    feat: rebrand HardAI

--- src/ChatInterface.test.tsx
+++ src/ChatInterface.test.tsx
@@ -53,9 +53,7 @@ describe('ChatInterface', () => {
   it('renders correctly with inactive conversation', () => {
     render(<ChatInterface {...mockProps} />);
 
-    const hardAILogo = screen.getByRole('img', {
-      name: 'Hard AI logo',
-    });
+    const hardAILogo = screen.getByTestId('kava-ai-logo');
     const welcomeText = screen.getByText('How can I help you with Web3?');
     const input = screen.getByPlaceholderText('Ask anything...');
 
--- src/LandingContent.tsx
+++ src/LandingContent.tsx
@@ -1,11 +1,11 @@
 import styles from './LandingContent.module.css';
-import hardAILogo from './assets/hardAILogo.svg';
+import KavaAILogo from './kavaAILogo';
 
 export const LandingContent = () => {
   return (
     <div className={styles.landingContent}>
       <div className={styles.logoWrapper}>
-        <img height={50} width={200} src={hardAILogo} alt="Hard AI logo" />
+        <KavaAILogo height={50} width={200} />
         <h2 className={styles.introText}>How can I help you with Web3?</h2>
       </div>
     </div>
