ac950d99cf27e09866cdc82777c1628b34c5a84f
Robert Pirtle <Astropirtle@gmail.com>
2025-07-25 10:00:03 -0700

    refactor: extract reusable modal component (#130)

--- src/WalletModal.tsx
+++ src/WalletModal.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styles from './WalletModal.module.css';
-import { ButtonIcon } from 'lib-kava-ai';
-import { X } from 'lucide-react';
+import Modal from './components/Modal';
 import { useWalletState } from './stores/walletStore/useWalletState';
 import { EIP6963ProviderDetail } from './stores/walletStore';
 import { PROMOTED_WALLETS } from './utils/wallet';
@@ -13,7 +12,6 @@ interface WalletModalProps {
 const WalletModal: React.FC<WalletModalProps> = ({ onClose }) => {
   const { availableProviders, handleProviderSelect } = useWalletState();
   const hasProviders = availableProviders.length > 0;
-  const modalRef = useRef<HTMLDivElement>(null);
 
   //  If a user doesn't have one of the promoted wallets install, display it as a link
   //  so they can download if they want
@@ -29,72 +27,47 @@ const WalletModal: React.FC<WalletModalProps> = ({ onClose }) => {
     onClose();
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
-      ) {
-        onClose();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [onClose]);
-
   return (
-    <div className={styles.providerModalBackdrop}>
-      <div className={styles.providerModal} ref={modalRef}>
-        <div className={styles.modalHeader}>
-          <h3>{hasProviders ? 'Select a Wallet' : 'Get a Wallet'}</h3>
-          <ButtonIcon
-            icon={X}
-            aria-label={'Close wallet connect'}
-            onClick={onClose}
-          />
-        </div>
-
-        <div className={styles.providersList}>
-          {hasProviders &&
-            availableProviders.map((provider) => (
-              <div
-                key={provider.info.uuid}
-                className={styles.walletOption}
-                onClick={() => selectProvider(provider)}
-              >
-                <div className={styles.walletOptionIcon}>
-                  <img
-                    src={provider.info.icon}
-                    alt={`${provider.info.name} icon`}
-                  />
-                </div>
-                <div className={styles.walletName}>{provider.info.name}</div>
-              </div>
-            ))}
-          {missingPromotedWallets.map((wallet) => (
-            <a
-              key={wallet.name}
-              href={wallet.downloadUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.walletLink}
+    <Modal
+      title={hasProviders ? 'Select a Wallet' : 'Get a Wallet'}
+      onClose={onClose}
+    >
+      <div className={styles.providersList}>
+        {hasProviders &&
+          availableProviders.map((provider) => (
+            <div
+              key={provider.info.uuid}
+              className={styles.walletOption}
+              onClick={() => selectProvider(provider)}
             >
-              <div className={styles.walletOption}>
-                <div className={styles.walletOptionIcon}>
-                  <img src={wallet.logo} alt={`${wallet.name} logo`} />
-                </div>
-                <div className={styles.walletName}>{wallet.name}</div>
-                <div className={styles.getButton}>Get</div>
+              <div className={styles.walletOptionIcon}>
+                <img
+                  src={provider.info.icon}
+                  alt={`${provider.info.name} icon`}
+                />
               </div>
-            </a>
+              <div className={styles.walletName}>{provider.info.name}</div>
+            </div>
           ))}
-        </div>
+        {missingPromotedWallets.map((wallet) => (
+          <a
+            key={wallet.name}
+            href={wallet.downloadUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.walletLink}
+          >
+            <div className={styles.walletOption}>
+              <div className={styles.walletOptionIcon}>
+                <img src={wallet.logo} alt={`${wallet.name} logo`} />
+              </div>
+              <div className={styles.walletName}>{wallet.name}</div>
+              <div className={styles.getButton}>Get</div>
+            </div>
+          </a>
+        ))}
       </div>
-    </div>
+    </Modal>
   );
 };
 
